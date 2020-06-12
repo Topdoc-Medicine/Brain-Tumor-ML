10df8d149c57d0f541f818078c06afa3bfb536fd
Howard Huang <howardhuang2004@gmail.com>
2020-06-11 18:36:44 -0700

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -35,6 +35,28 @@ for CLASS in os.listdir(IMG_PATH):
             else:
                 shutil.copy(img, 'VAL/'+ CLASS.upper() + '/' + FILE_NAME)
 
+def load_data(dir_path, img_size=(100,100)):
+    """
+    Load resized images as np.arrays to workspace
+    """
+    X = []
+    y = []
+    i = 0
+    labels = dict()
+    for path in tqdm(sorted(os.listdir(dir_path))):
+        if not path.startswith('.'):
+            labels[i] = path
+            for file in os.listdir(dir_path + path):
+                if not file.startswith('.'):
+                    img = cv2.imread(dir_path + path + '/' + file)
+                    X.append(img)
+                    y.append(i)
+            i += 1
+    X = np.array(X)
+    y = np.array(y)
+    print(f'{len(X)} images loaded from {dir_path} directory.')
+    return X, y, labels
+
 TRAIN_DIR = 'TRAIN/'
 TEST_DIR = 'TEST/'
 VAL_DIR = 'VAL/'
@@ -45,79 +67,142 @@ X_train, y_train, labels = load_data(TRAIN_DIR, IMG_SIZE)
 X_test, y_test, _ = load_data(TEST_DIR, IMG_SIZE)
 X_val, y_val, _ = load_data(VAL_DIR, IMG_SIZE)
 
-plot_samples(X_train, y_train, labels, 30)
+y = dict()
+y[0] = []
+y[1] = []
+for set_name in (y_train, y_val, y_test):
+    y[0].append(np.sum(set_name == 0))
+    y[1].append(np.sum(set_name == 1))
+
+trace0 = go.Bar(
+    x=['Train Set', 'Validation Set', 'Test Set'],
+    y=y[0],
+    name='No',
+    marker=dict(color='#33cc33'),
+    opacity=0.7
+)
+trace1 = go.Bar(
+    x=['Train Set', 'Validation Set', 'Test Set'],
+    y=y[1],
+    name='Yes',
+    marker=dict(color='#ff3300'),
+    opacity=0.7
+)
+data = [trace0, trace1]
+layout = go.Layout(
+    title='Count of classes in each set',
+    xaxis={'title': 'Set'},
+    yaxis={'title': 'Count'}
+)
+fig = go.Figure(data, layout)
+iplot(fig)
+
+def crop_imgs(set_name, add_pixels_value=0):
+    """
+    Finds the extreme points on the image and crops the rectangular out of them
+    """
+    set_new = []
+    for img in set_name:
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        gray = cv2.GaussianBlur(gray, (5, 5), 0)
+
+        # threshold the image, then perform a series of erosions +
+        # dilations to remove any small regions of noise
+        thresh = cv2.threshold(gray, 45, 255, cv2.THRESH_BINARY)[1]
+        thresh = cv2.erode(thresh, None, iterations=2)
+        thresh = cv2.dilate(thresh, None, iterations=2)
+
+        # find contours in thresholded image, then grab the largest one
+        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        c = max(cnts, key=cv2.contourArea)
+
+        # find the extreme points
+        extLeft = tuple(c[c[:, :, 0].argmin()][0])
+        extRight = tuple(c[c[:, :, 0].argmax()][0])
+        extTop = tuple(c[c[:, :, 1].argmin()][0])
+        extBot = tuple(c[c[:, :, 1].argmax()][0])
+
+        ADD_PIXELS = add_pixels_value
+        new_img = img[extTop[1]-ADD_PIXELS:extBot[1]+ADD_PIXELS, extLeft[0]-ADD_PIXELS:extRight[0]+ADD_PIXELS].copy()
+        set_new.append(new_img)
+
+    return np.array(set_new)
+
+img = cv2.imread('../input/brain-mri-images-for-brain-tumor-detection/brain_tumor_dataset/yes/Y108.jpg')
+img = cv2.resize(
+            img,
+            dsize=IMG_SIZE,
+            interpolation=cv2.INTER_CUBIC
+        )
+gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+gray = cv2.GaussianBlur(gray, (5, 5), 0)
+
+# threshold the image, then perform a series of erosions +
+# dilations to remove any small regions of noise
+thresh = cv2.threshold(gray, 45, 255, cv2.THRESH_BINARY)[1]
+thresh = cv2.erode(thresh, None, iterations=2)
+thresh = cv2.dilate(thresh, None, iterations=2)
+
+# find contours in thresholded image, then grab the largest one
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+c = max(cnts, key=cv2.contourArea)
+
+# find the extreme points
+extLeft = tuple(c[c[:, :, 0].argmin()][0])
+extRight = tuple(c[c[:, :, 0].argmax()][0])
+extTop = tuple(c[c[:, :, 1].argmin()][0])
+extBot = tuple(c[c[:, :, 1].argmax()][0])
+
+# add contour on the image
+img_cnt = cv2.drawContours(img.copy(), [c], -1, (0, 255, 255), 4)
+
+# add extreme points
+img_pnt = cv2.circle(img_cnt.copy(), extLeft, 8, (0, 0, 255), -1)
+img_pnt = cv2.circle(img_pnt, extRight, 8, (0, 255, 0), -1)
+img_pnt = cv2.circle(img_pnt, extTop, 8, (255, 0, 0), -1)
+img_pnt = cv2.circle(img_pnt, extBot, 8, (255, 255, 0), -1)
+
+# crop
+ADD_PIXELS = 0
+new_img = img[extTop[1]-ADD_PIXELS:extBot[1]+ADD_PIXELS, extLeft[0]-ADD_PIXELS:extRight[0]+ADD_PIXELS].copy()
 
 # apply this for each set
 X_train_crop = crop_imgs(set_name=X_train)
 X_val_crop = crop_imgs(set_name=X_val)
 X_test_crop = crop_imgs(set_name=X_test)
 
-plot_samples(X_train_crop, y_train, labels, 30)
-
-# saving new images to the folder
-path = './brain-mri-images-for-brain-tumor-detection/brain_tumor_dataset/TRAIN_CROP/YES'
-
-try:
-    os.makedirs(path)
-except OSError:
-    print ("creation of the directory %s failed" % path)
-else:
-    print("successfully created the directory %s" % path)
-
-path = './brain-mri-images-for-brain-tumor-detection/brain_tumor_dataset/TRAIN_CROP/NO'
-
-try:
-    os.makedirs(path)
-except OSError:
-    print ("creation of the directory %s failed" % path)
-else:
-    print("successfully created the directory %s" % path)
-
-path = './brain-mri-images-for-brain-tumor-detection/brain_tumor_dataset/TEST_CROP/YES'
-
-try:
-    os.makedirs(path)
-except OSError:
-    print ("creation of the directory %s failed" % path)
-else:
-    print("successfully created the directory %s" % path)
-
-path = './brain-mri-images-for-brain-tumor-detection/brain_tumor_dataset/TEST_CROP/NO'
-
-try:
-    os.makedirs(path)
-except OSError:
-    print ("creation of the directory %s failed" % path)
-else:
-    print("successfully created the directory %s" % path)
-
-path = './brain-mri-images-for-brain-tumor-detection/brain_tumor_dataset/VAL_CROP/YES'
-
-try:
-    os.makedirs(path)
-except OSError:
-    print ("creation of the directory %s failed" % path)
-else:
-    print("successfully created the directory %s" % path)
-
-path = './brain-mri-images-for-brain-tumor-detection/brain_tumor_dataset/VAL_CROP/NO'
-
-try:
-    os.makedirs(path)
-except OSError:
-    print ("creation of the directory %s failed" % path)
-else:
-    print("successfully created the directory %s" % path)
+def save_new_images(x_set, y_set, folder_name):
+    i = 0
+    for (img, imclass) in zip(x_set, y_set):
+        if imclass == 0:
+            cv2.imwrite(folder_name+'NO/'+str(i)+'.jpg', img)
+        else:
+            cv2.imwrite(folder_name+'YES/'+str(i)+'.jpg', img)
+        i += 1
 
 save_new_images(X_train_crop, y_train, folder_name='TRAIN_CROP/')
 save_new_images(X_val_crop, y_val, folder_name='VAL_CROP/')
 save_new_images(X_test_crop, y_test, folder_name='TEST_CROP/')
 
+def preprocess_imgs(set_name, img_size):
+    """
+    Resize and apply VGG-15 preprocessing
+    """
+    set_new = []
+    for img in set_name:
+        img = cv2.resize(
+            img,
+            dsize=img_size,
+            interpolation=cv2.INTER_CUBIC
+        )
+        set_new.append(preprocess_input(img))
+    return np.array(set_new)
+
 X_train_prep = preprocess_imgs(set_name=X_train_crop, img_size=IMG_SIZE)
 X_test_prep = preprocess_imgs(set_name=X_test_crop, img_size=IMG_SIZE)
 X_val_prep = preprocess_imgs(set_name=X_val_crop, img_size=IMG_SIZE)
-
-plot_samples(X_train_prep, y_train, labels, 30)
 
 # set the paramters we want to change randomly
 demo_datagen = ImageDataGenerator(
@@ -130,6 +215,16 @@ demo_datagen = ImageDataGenerator(
     horizontal_flip=True,
     vertical_flip=True
 )
+
+os.mkdir('preview')
+x = X_train_crop[0]
+x = x.reshape((1,) + x.shape)
+
+i = 0
+for batch in demo_datagen.flow(x, batch_size=1, save_to_dir='preview', save_prefix='aug_img', save_format='jpg'):
+    i += 1
+    if i > 20:
+        break 
 
 TRAIN_DIR = 'TRAIN_CROP/'
 VAL_DIR = 'VAL_CROP/'
